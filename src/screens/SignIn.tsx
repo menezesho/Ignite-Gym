f7aeb4cb9ab3a2432ba2b23147659852d980bafa
Henrique Menezes <henryyquemenezes@hotmail.com>
2024-10-08 16:50:47 -0300

    fix - erro de imagem de fundo descentralizada

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,21 +1,22 @@
+
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
 
-import { Image, VStack, Text, Center, Heading, ScrollView } from "native-base";
+import { Image, VStack, Text, Center, Heading, View } from "native-base";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignIn() {
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-      <VStack flex={1} bg={'gray.700'} px={4}>
-        <Image
-          source={BackgroundImg}
-          alt='Pessoas treinando'
-          resizeMode='cover'
-          position='absolute'
-        />
+    <VStack flex={1} bg={'gray.700'}>
+      <Image
+        source={BackgroundImg}
+        alt='Pessoas treinando'
+        resizeMode='cover'
+        position='absolute'
+      />
 
+      <View px={4}>
         <Center my={24}>
           <LogoSvg />
 
@@ -35,6 +36,7 @@ export function SignIn() {
             autoCapitalize='none'
             autoCorrect={false}
           />
+
           <Input
             placeholder='Senha'
             secureTextEntry
@@ -50,7 +52,7 @@ export function SignIn() {
 
           <Button title='Criar conta' variant='outline' />
         </Center>
-      </VStack>
-    </ScrollView>
+      </View>
+    </VStack>
   );
 }
