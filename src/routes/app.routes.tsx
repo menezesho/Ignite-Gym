f9881b82de899c8ed3734c80861c7426a64339f8
Henrique Menezes <henryyquemenezes@hotmail.com>
2024-10-08 18:26:06 -0300

    feat - estilização da bottom nav bar

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,13 +3,13 @@ import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-naviga
 import HomeSvg from '@assets//home.svg';
 import HistorySvg from '@assets/history.svg';
 import ProfileSvg from '@assets/profile.svg';
-import ExerciseSvg from '@assets/exercise.svg';
 
 import { Home } from '@screens/Home';
 import { History } from '@screens/History';
 import { Profile } from '@screens/Profile';
 import { Exercise } from '@screens/Exercise';
 import { useTheme } from 'native-base';
+import { Platform } from 'react-native';
 
 type AppRoutes = {
   home: undefined;
@@ -23,13 +23,22 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
-  const { sizes } = useTheme();
+  const { sizes, colors } = useTheme();
   const iconSize = sizes[6];
 
   return (
     <Navigator screenOptions={{
       headerShown: false,
       tabBarShowLabel: false,
+      tabBarActiveTintColor: colors.green[500],
+      tabBarInactiveTintColor: colors.gray[200],
+      tabBarStyle: {
+        backgroundColor: colors.gray[600],
+        borderTopWidth: 0,
+        // height: Platform.OS === 'android' ? 'auto' : 96,
+        // paddingBottom: sizes[10],
+        // paddingTop: sizes[6]
+      }
     }}>
       <Screen
         name='home'
@@ -64,6 +73,7 @@ export function AppRoutes() {
       <Screen
         name='exercise'
         component={Exercise}
+        options={{ tabBarButton: () => null }}
       />
     </Navigator>
   );
