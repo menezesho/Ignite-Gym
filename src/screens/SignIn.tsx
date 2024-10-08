686d9f34f06f88b212ba405a283f5a6ef841c5b9
Henrique Menezes <henryyquemenezes@hotmail.com>
2024-10-08 17:48:00 -0300

    feat - navegação de SignIn e SignUp

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,26 @@
+import { useNavigation } from '@react-navigation/native';
+import { Image, VStack, Text, Center, Heading, View } from "native-base";
+
+import type { AuthNavigatorRoutesProps } from '@routes/auth.routes';
 
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
 
-import { Image, VStack, Text, Center, Heading, View } from "native-base";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignIn() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  function handleNewAccount() {
+    navigation.navigate('signUp');
+  }
+
   return (
     <VStack flex={1}>
       <Image
         source={BackgroundImg}
+        defaultSource={BackgroundImg}
         alt='Pessoas treinando'
         resizeMode='cover'
         position='absolute'
@@ -50,7 +60,11 @@ export function SignIn() {
             Ainda não tem acesso?
           </Text>
 
-          <Button title='Criar conta' variant='outline' />
+          <Button
+            title='Criar conta'
+            variant='outline'
+            onPress={handleNewAccount}
+          />
         </Center>
       </View>
     </VStack>
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,16 +1,26 @@
+import { useNavigation } from '@react-navigation/native';
+import { Image, VStack, Text, Center, Heading, View } from "native-base";
+
+import type { AuthNavigatorRoutesProps } from '@routes/auth.routes';
 
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
 
-import { Image, VStack, Text, Center, Heading, View } from "native-base";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignUp() {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <VStack flex={1}>
       <Image
         source={BackgroundImg}
+        defaultSource={BackgroundImg}
         alt='Pessoas treinando'
         resizeMode='cover'
         position='absolute'
@@ -49,7 +59,11 @@ export function SignUp() {
           <Button title='Criar e acessar' />
         </Center>
 
-        <Button title='Voltar para o login' variant='outline' mt={24} />
+        <Button
+          title='Voltar para o login'
+          variant='outline' mt={24}
+          onPress={handleGoBack}
+        />
       </View>
     </VStack>
   );
