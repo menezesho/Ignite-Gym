87357bc5d77008aeb5e164a9688bf95e5e9d4bb7
Henrique Menezes <henryyquemenezes@hotmail.com>
2024-10-08 18:44:19 -0300

    criação do Group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,14 @@
 import { VStack } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
+import { Group } from "@components/Group";
 
 export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <Group name='costas' />
     </VStack>
   );
 }
