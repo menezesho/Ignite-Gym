8992847f223e3484704cd0834d031ae49a96b32b
Henrique Menezes <henryyquemenezes@hotmail.com>
2024-10-09 23:10:09 -0300

    feat - desenvolvimento da home screen

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,49 @@
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { Heading, HStack, Image, VStack, Text, Icon } from "native-base";
+
+import { MaterialIcons } from '@expo/vector-icons';
+
+type Props = TouchableOpacityProps & {
+  name: string;
+  image: string;
+  description: string;
+}
+
+export function ExerciseCard({ name, image, description, ...rest }: Props) {
+  return (
+    <TouchableOpacity {...rest}>
+      <HStack bg='gray.500' alignItems='center' px={2} pr={4} rounded='md' mb={3}>
+        <Image
+          source={{ uri: image }}
+          alt='Imagem do exercício'
+          w={16}
+          h={16}
+          rounded='md'
+          mr={4}
+          resizeMode='center'
+        />
+
+        <VStack flex={1}>
+          <Heading fontSize='lg' color='white'>
+            {name}
+          </Heading>
+
+          <Text
+            fontSize='sm'
+            color='gray.200'
+            mt={1}
+            numberOfLines={2}
+          >
+            {description}
+          </Text>
+        </VStack>
+
+        <Icon
+          as={MaterialIcons}
+          name='chevron-right'
+          color='gray.300'
+        />
+      </HStack>
+    </TouchableOpacity>
+  );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,61 @@
-import { VStack } from "native-base";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
+import { useState } from "react";
+import { ExerciseCard } from "@components/ExerciseCard";
 
 export function Home() {
+  const [selectedGroup, setSelectedGroup] = useState('costas');
+  const [groups, setGroups] = useState(['costas', 'ombros', 'biceps', 'triceps', 'peito', 'pernas', 'abdomen']);
+  const [exercises, setExercises] = useState(['Remada lateral', 'Puxada frontal', 'Remada unilateral']);
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name='costas' />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={selectedGroup === item}
+            onPress={() => setSelectedGroup(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent='space-between' mb={5}>
+          <Heading color='gray.200' fontSize='md'>
+            Exercícios
+          </Heading>
+
+          <Text color='gray.200' fontSize='sm'>
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard
+              name={item}
+              image='https://conteudo.imguol.com.br/c/entretenimento/f9/2019/08/27/remada-na-maquina-1566927337178_v2_1036x685.jpg'
+              description='3 séries de 10 repetições'
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
     </VStack>
   );
 }
