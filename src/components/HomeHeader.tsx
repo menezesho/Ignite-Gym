f1ec0057c2c2968969c6a61c24e1b3b2a5ab7741
Henrique Menezes <henryyquemenezes@hotmail.com>
2024-10-16 11:54:31 -0300

    feat - criação do componente de foto do usuário

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,10 +2,18 @@ import { Heading, VStack, HStack, Text, Icon } from "native-base";
 
 import { MaterialIcons } from '@expo/vector-icons';
 import { TouchableOpacity } from "react-native";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
     <HStack bg='gray.600' pt={16} pb={5} px={8} alignItems='center'>
+      <UserPhoto
+        source={{ uri: 'https://github.com/menezesho.png' }}
+        alt='Foto de perfil do usuário'
+        size={16}
+        mr={4}
+      />
+
       <VStack flex={1}>
         <Text color='gray.100' fontSize='md'>
           Olá
