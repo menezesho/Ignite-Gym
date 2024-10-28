4b5a66b48ebe9e2251171f17f82e93095808abe9
Henrique Menezes <82477364+menezesho@users.noreply.github.com>
2024-10-28 19:43:18 -0300

    feat - validação da confirmação de senha

--- src/utils/schemas/sign-up.schema.ts
+++ src/utils/schemas/sign-up.schema.ts
@@ -4,5 +4,5 @@ export const signUpSchema = yup.object({
   name: yup.string().required('Informe o nome'),
   email: yup.string().required('Informe o e-mail').email('E-mail inválido'),
   password: yup.string().required('Informe a senha').min(8, 'A senha deve ter no mínimo 8 caracteres'),
-  passwordConfirmation: yup.string().required('Informe a confirmação da senha')
+  passwordConfirmation: yup.string().required('Informe a confirmação da senha').oneOf([yup.ref('password')], 'A confirmação da senha está incorreta')
 });
