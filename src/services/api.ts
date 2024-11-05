efb053a07864574afee86d153d17572b8827db7b
Henrique Menezes <82477364+menezesho@users.noreply.github.com>
2024-11-04 23:27:03 -0300

    feat - signout de usuário sem token válido

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,32 @@
+import axios, { AxiosInstance } from 'axios';
 import { AppError } from '@utils/AppError';
-import axios from 'axios';
+
+type SignOut = () => void;
+
+type ApiInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void;
+}
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.105:3333',
-});
+  baseURL: 'http://192.168.15.101:3333',
+}) as ApiInstanceProps;
 
-api.interceptors.response.use(response => response, error => {
-  if (error.response && error.response.data) {
-    return Promise.reject(new AppError(error.response.data.message));
-  } else {
-    return Promise.reject(error);
+api.registerInterceptTokenManager = signOut => {
+  const interceptTokenManager = api.interceptors.response.use(response => response, requestError => {
+    if (requestError?.response?.status === 401) {
+      signOut();
+    }
+
+    if (requestError.response && requestError.response.data) {
+      return Promise.reject(new AppError(requestError.response.data.message));
+    } else {
+      return Promise.reject(requestError);
+    }
+  });
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager);
   }
-});
+}
 
 export { api };
